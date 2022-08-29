a3663e9906d36a779a0b0faee912c9cd8a4268c3
Igor Gabriel <97804247+igorUfca@users.noreply.github.com>
2022-08-29 15:58:51 -0300

    atualização exercicio

--- exercicio3.sh
+++ exercicio3.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+#recebe o caminho de um diretóriio e listar o conteúdo um a um. se for diretorio, escrever "(dir)" ao lado do diretorio.
+
 dir=$1
 
 [ ! -d "$dir" ] && echo "dir n existe pow" && exit
